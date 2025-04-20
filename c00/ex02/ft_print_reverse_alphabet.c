f99c162b05d2993888bd8e9b2e22c605c809b017
Gustavo Souza <91276135+Gus1331@users.noreply.github.com>
2025-04-20 03:13:10 +0000

    feat: c00

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustaoli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 13:54:35 by gustaoli          #+#    #+#             */
+/*   Updated: 2025/04/18 13:58:12 by gustaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z != ('a' - 1))
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
